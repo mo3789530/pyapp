8fc21b263cfee504365c97a829fa338a3d077c5d
mo3789530 <mo3789530@gmail.com>
2025-07-27 23:26:46 +0900

    add  security

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -2,7 +2,9 @@ from fastapi import Request, HTTPException, status, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from functools import wraps
 
-SECRET_KEY = "your-secret-key"
+import jwt
+
+SECRET_KEY = "a-string-secret-at-least-256-bits-long"
 ALGORITHM = "HS256"
 bearer_scheme = HTTPBearer()
 
@@ -13,10 +15,21 @@ def get_current_user(
 ):
     token = credentials.credentials
     print(token, action)
-    if token == "test":
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        print(username)
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except Exception as e:
+        print(e)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    return token
+    return payload
